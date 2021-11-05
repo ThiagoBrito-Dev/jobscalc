a1fcdd551d7543afd9ee3111aa37e0c5f1fb93d8
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2021-11-04 21:10:09 -0300

    Template engine

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const router = new express.Router();
 
+const basePath = `${__dirname}/views`;
+
+const profile = {
+  name: "Thiago Brito",
+  avatar: "https://github.com/trybrito.png",
+  "monthly-budget": "R$ 4.000,00",
+  "hours-per-day": 8,
+  "days-per-week": 6,
+  "vacation-per-year": 4,
+};
+const jobs = [];
+
 router.get("/", (request, response) => {
-  return response.sendFile(`${__dirname}/views/index.html`);
+  return response.render(`${basePath}/index`);
+});
+
+router.get("/job", (request, response) => {
+  return response.render(`${basePath}/job`);
+});
+router.post("/job", (request, response) => {
+  jobs.push(request.body);
+  console.log(jobs);
+  return response.redirect("/");
+});
+
+router.get("/job/edit", (request, response) => {
+  return response.render(`${basePath}/job-edit`);
+});
+router.get("/profile", (request, response) => {
+  return response.render(`${basePath}/profile`, { profile });
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = require("./routes");
 const server = new express();
 
+server.set("view engine", "ejs");
 server.use(express.static("public"));
+server.use(express.urlencoded());
 server.use(router);
 
-server.listen(3000, () => console.log("Server is running"));
+server.listen(3000, () =>
+  console.log("Server is running at <http://localhost:3000>")
+);
