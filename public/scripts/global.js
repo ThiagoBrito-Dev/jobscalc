c54ec73720c6ac300b84239689e72be6277b86a7
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2021-11-14 17:51:16 -0300

    Theme switcher

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -1,17 +1,26 @@
 const htmlTag = document.querySelector("html");
 const themeSwitcher = document.getElementById("theme-switcher");
+const themeIcon = document.getElementById("theme-icon");
 const appTheme = localStorage.getItem("jobscalc:appTheme");
 
 if (appTheme) {
   htmlTag.classList.add(appTheme);
   themeSwitcher.classList.add(appTheme);
+  themeIcon.src = "/images/moon.svg";
+  themeIcon.alt = "Ícone de lua";
 }
 
 themeSwitcher.onclick = () => {
   htmlTag.classList.toggle("dark-theme");
   themeSwitcher.classList.toggle("dark-theme");
 
-  htmlTag.classList.value.includes("dark-theme")
-    ? localStorage.setItem("jobscalc:appTheme", "dark-theme")
-    : localStorage.setItem("jobscalc:appTheme", "");
+  if (htmlTag.classList.value.includes("dark-theme")) {
+    localStorage.setItem("jobscalc:appTheme", "dark-theme");
+    themeIcon.src = "/images/moon.svg";
+    themeIcon.alt = "Ícone de lua";
+  } else {
+    localStorage.setItem("jobscalc:appTheme", "");
+    themeIcon.src = "/images/sun.svg";
+    themeIcon.alt = "Ícone de sol";
+  }
 };
