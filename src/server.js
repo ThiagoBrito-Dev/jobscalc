ff3a0f62898069b42144036e2dfbafc356a36b5c
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2021-11-30 16:36:10 -0300

    Bugfix

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const server = new express();
 server.set("view engine", "ejs");
 server.set("views", path.join(__dirname, "views"));
 server.use(express.static("public"));
-server.use(express.urlencoded());
+server.use(express.urlencoded({ extended: true }));
 server.use(router);
 
 server.listen(3000, () =>
